3aa30731c5f1ef83bcee5dbdb44ae8081e030aaa
Mahdi Ebrahimi <azkia@iran.ir>
2020-10-01 13:42:57 +0330

    install font shabnam

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import fullpage from 'fullpage.js';
+
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true
+});
+
+//methods
+// fullpage_api.setAllowScrolling(false);
